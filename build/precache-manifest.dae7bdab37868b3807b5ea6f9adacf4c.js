70dd0ade6bc49ce251a2b24d48926e5963d5013e
iamjerriz <56684439+iamjerriz@users.noreply.github.com>
2021-01-06 11:46:59 +0800

    Add files via upload
    Build Folder of Vlogdat-Client

---
+++ build/precache-manifest.dae7bdab37868b3807b5ea6f9adacf4c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84d8b54c5d8e3f52fad22966e555a9b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4512f30f3450fe0ec523",
+    "url": "/static/css/main.834a0770.chunk.css"
+  },
+  {
+    "revision": "23bde18b3f483c2779d6",
+    "url": "/static/js/2.3e00aa03.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.3e00aa03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4512f30f3450fe0ec523",
+    "url": "/static/js/main.9259bb4b.chunk.js"
+  },
+  {
+    "revision": "ac07b1a83d8a659c22d4",
+    "url": "/static/js/runtime-main.f31ed0d1.js"
+  },
+  {
+    "revision": "5a021ab95b4d2a0c8e7511c40ca7bb2f",
+    "url": "/static/media/no-image.5a021ab9.png"
+  }
+]);
